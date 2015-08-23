acc1c238ef51a492371001332e09cecdb1e50d1b
Brenton "B-Train" Fletcher <i@bloople.net>
2015-08-23 23:13:19 +1000

    Compile haml and sass correctly

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@
 // Require the needed packages
 var gulp = require('gulp');
 var haml = require('gulp-haml');
+var sass = require('gulp-sass');
 
 var paths = {
-  haml: ['app/**.haml']
+  haml: ['app/**/*.haml'],
+  sass: ['app/**/*.scss']
 };
 
 gulp.task('haml', function() {
   gulp.src(paths.haml).pipe(haml()).pipe(gulp.dest('./dist'));
 });
 
+gulp.task('sass', function () {
+  gulp.src(paths.sass).pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./dist'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.haml, ['haml']);
+  gulp.watch(paths.sass, ['sass']);
 });
 
 gulp.task('default', ['watch']);
