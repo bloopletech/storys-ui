79bd01f8231352056401b4ff3d7348c31d7051d3
Brenton "B-Train" Fletcher <i@bloople.net>
2015-08-24 01:48:54 +1000

    bower drools, npm rools

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var haml = require('gulp-haml');
 var sass = require('gulp-sass');
-var bower = require('gulp-bower');
+var filter = require('gulp-filter');
 
 var paths = {
   haml: ['app/**/*.haml'],
   sass: ['app/**/*.scss'],
   js: ['app/**/*.js']
 };
+
+var vendoredPaths = [
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/underscore/underscore-min.js',
+  'node_modules/semantic-ui/dist/semantic.min.js',
+  'node_modules/semantic-ui/dist/semantic.min.css'
+];
 
 gulp.task('haml', function() {
   gulp.src(paths.haml).pipe(haml()).pipe(gulp.dest('./public'));
@@ -21,14 +28,15 @@ gulp.task('js', function() {
   gulp.src(paths.js).pipe(gulp.dest('./public'));
 });
 
-gulp.task('bower', function() {
-  bower().pipe(gulp.dest('./public/js'))
-});
-
 gulp.task('watch', function() {
   gulp.watch(paths.haml, ['haml']);
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.js, ['js']);
 });
 
-gulp.task('default', ['watch', 'haml', 'sass', 'js', 'bower']);
+gulp.task('vendored', function() {
+  gulp.src(vendoredPaths).pipe(filter('*.css')).pipe(gulp.dest('./public/css'));
+  gulp.src(vendoredPaths).pipe(filter('*.js')).pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('default', ['watch', 'haml', 'sass', 'js', 'vendored']);
